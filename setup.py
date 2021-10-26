c79839bb0d582f5e9340d47ef232027cbcc66441
Hboni <27290853+Hboni@users.noreply.github.com>
2021-10-26 09:58:05 +0200

    fixes module version

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
 from setuptools import find_packages, setup
+import deidentification
 
 # Select appropriate modules
 modules = find_packages()
@@ -9,7 +10,7 @@ setup(
     description='CATI dicom deidentification tool',
     author='CATI team',
     author_email='support@cati-neuroimaging.com',
-    version='0.1.0',
+    version=deidentification.__version__,
     script=['bin/deidentification'],
     packages=modules,
     install_requires=['pydicom'],
