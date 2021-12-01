87b1353b071d7263c3770250f47dd843b04f8f91
Hboni <27290853+Hboni@users.noreply.github.com>
2021-12-01 09:19:47 +0100

    Update tag representation in tsv config files

--- deidentification/tag_lists.py
+++ deidentification/tag_lists.py
@@ -1,19 +1,21 @@
 import csv
 
 from deidentification import CONFIG_FOLDER
+from deidentification.config import tag_to_tuple
 
 conf_profile_path = CONFIG_FOLDER.joinpath('confidentiality_profiles.tsv')
 conf_profile = {}
 conf_profile_range = {}
 with open(conf_profile_path, 'r') as tsv_file:
     tsv_reader = csv.reader(tsv_file, delimiter='\t')
+    header = next(tsv_reader)
     for d in tsv_reader:
         if 'X' not in d[0]:
-            conf_profile[eval(d[0])] = {'name': d[2], 'profile': d[1]}
+            conf_profile[tag_to_tuple(d[0])] = {'name': d[2], 'profile': d[1]}
         else:
             range_key = (
-                eval(d[0].replace('X', '0')),
-                eval(d[0].replace('X', 'F'))
+                tag_to_tuple(d[0].replace('X', '0')),
+                tag_to_tuple(d[0].replace('X', 'F'))
             )
             conf_profile_range[range_key] = {'name': d[2], 'profile': d[1]}
 
@@ -24,4 +26,4 @@ with open(safe_private_path, 'r') as tsv_file:
     header = next(tsv_reader)
     for d in tsv_reader:
         safe_private_attributes.setdefault(d[1], [])
-        safe_private_attributes[d[1]].append(d[0])
+        safe_private_attributes[d[1]].append(tag_to_tuple(d[0]))
