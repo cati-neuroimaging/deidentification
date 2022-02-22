8a3b3fb4405cb0394dfb33169c34852f33fad14d
Hugo Boniface <27290853+Hboni@users.noreply.github.com>
2022-02-22 17:24:53 +0100

    create info.py file

---
+++ deidentification/info.py
@@ -0,0 +1,14 @@
+__version__ = '1.0.0'
+
+# Main setup parameters
+NAME = 'deidentification'
+DESCRIPTION = 'CATI dicom deidentification tool'
+PROJECT = 'deidentification'
+ORGANISATION = 'CATI'
+AUTHOR = 'CATI'
+LICENSE = 'MIT License'
+AUTHOR_EMAIL = 'support@cati-neuroimaging.com'
+VERSION = __version__
+REQUIRES = ['pydicom']
+
+brainvisa_build_model = 'pure_python'
