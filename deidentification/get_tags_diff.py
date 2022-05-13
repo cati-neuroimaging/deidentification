8281c1d9a46c6d686d6062136012831d158243c0
Remy Bregand <remy.bregand@icm-institute.org>
2022-05-13 11:15:42 +0200

    fix display error

--- deidentification/get_tags_diff.py
+++ deidentification/get_tags_diff.py
@@ -11,52 +11,57 @@ def get_tag_diff(path_raw, path_ano):
     contain the tag difference between the 2 dcm 
 
     Args:
-        path_dcm1 (str): path of the first dcm
-        path_dcm2 (str): path of the second dcm
+        path_dcm1 (str): path of the first dcm (ref)
+        path_dcm2 (str): path of the second dcm (current)
     """
     print(f"Check difference between :\n{path_raw} and\n{path_ano}\n")
-    dcm1 = pydicom.read_file(path_raw)
-    dcm2 = pydicom.read_file(path_ano)
-    diff = difflib.ndiff(str(dcm1).splitlines(keepends=True),
-                         str(dcm2).splitlines(keepends=True))
+    dcm_ref = pydicom.read_file(path_raw)
+    dcm_current = pydicom.read_file(path_ano)
+    diff = difflib.ndiff(str(dcm_ref).splitlines(keepends=True),
+                         str(dcm_current).splitlines(keepends=True))
     
-    diff1 = []
-    diff2= []
-    modif = []
-    l = None
-    readed_tags = []
+    diff_from_dcm1 = []
+    diff_from_dcm_2 = []
+    global_modif = []
+    current_line_diff = None
+    previous_tag = None
+    previous_sign =  None
 
     for i in diff:
-        if ")" not in i or ")" not in i:
+        if "(" not in i or ")" not in i:
             continue
         tag = i[i.index('('): i.index(')')+1]
         txt = ""
-        if tag not in readed_tags:
-            readed_tags.append(tag)
-            
+
+        # tag present in only one dcm
+        if tag != previous_tag:
             if i[0] == "-":
                 txt = "Dcm1 only:"
-                l = diff1
+                current_line_diff = diff_from_dcm1
             elif i[0] == "+":
                 txt = "Dcm2 only:"
-                l = diff2
+                current_line_diff = diff_from_dcm_2
             i = txt + i[1:]
-        elif tag in readed_tags and i[0]== "+":
+        # between 2 diff lines : same tag + remove from first line and add to second -> value modification
+        elif tag == previous_tag and i[0] != previous_sign:
             txt = "Value modification (dcm1 -> dcm2): "
-            previous_value = l.pop(-1).split(': ')[-1].replace('\n', '')
-            i = txt + tag + " " + previous_value + " -----> " + i.split(': ')[-1].replace('\n', '') + '\n'
-            l = modif
+            previous_value = current_line_diff.pop(-1).split(': ')[-1].replace('\n', '')
+            tag_name = i[i.index(')') + 1: i.index('  ')]
+            i = txt + tag + tag_name + "\t\t" + previous_value + " -----> " + i.split(': ')[-1].replace('\n', '') + '\n'
+            current_line_diff = global_modif
         if txt != "":
-            l.append(i)
+            previous_tag = tag
+            previous_sign = i[0]
+            current_line_diff.append(i)
 
     with open(os.path.join(os.path.dirname(path_raw), "diff.txt"), "w") as f:
         f.write(f'dcm1 = {path_raw}\n')
         f.write(f'dcm2 = {path_ano}\n\n\n')
-        f.writelines(diff1)
+        f.writelines(diff_from_dcm1)
         f.write('\n\n')
-        f.writelines(diff2)
+        f.writelines(diff_from_dcm_2)
         f.write('\n\n')
-        f.writelines(modif)
+        f.writelines(global_modif)
     
     print("Done\n")
     print("Output file : " + os.path.join(os.path.dirname(path_raw), "diff.txt"))
